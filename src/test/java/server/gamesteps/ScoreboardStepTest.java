fed0725606d14529343ead5252f0d19007f3db6f
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-05-28 17:20:33 +0200

    feature(GameStep): Added tests for some GameSteps

---
+++ src/test/java/server/gamesteps/ScoreboardStepTest.java
@@ -0,0 +1,60 @@
+package server.gamesteps;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import questions.Question;
+import server.GameContext;
+import server.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class ScoreboardStepTest {
+    GameContext gameContext;
+    private List<Question> questions;
+    Player player1;
+    Player player2;
+    ScoreboardStep scoreboardStep;
+
+    @BeforeEach
+    void setUp() {
+        questions = new ArrayList<>();
+        player1 = mock(Player.class);
+        player2 = mock(Player.class);
+        when(player1.getId()).thenReturn(0);
+        when(player2.getId()).thenReturn(1);
+        gameContext = new GameContext(List.of(player1, player2), questions);
+        scoreboardStep = new ScoreboardStep();
+    }
+
+    @Test
+    void process_positive_simple() {
+        when(player1.getScore()).thenReturn(0);
+        when(player2.getScore()).thenReturn(1);
+
+        scoreboardStep.process(gameContext);
+
+        final String expectedScoreboard = """
+                Scoreboard for round 0:
+                1. Place: Player 1 with 1 Points
+                2. Place: Player 0 with 0 Points
+                """;
+        verify(player1).send(expectedScoreboard);
+        verify(player2).send(expectedScoreboard);
+    }
+
+    @Test
+    void process_positive_noPoints() {
+        scoreboardStep.process(gameContext);
+
+        final String expectedScoreboard = """
+                Scoreboard for round 0:
+                1. Place: Player 0 with 0 Points
+                2. Place: Player 1 with 0 Points
+                """;
+        verify(player1).send(expectedScoreboard);
+        verify(player2).send(expectedScoreboard);
+    }
+}
