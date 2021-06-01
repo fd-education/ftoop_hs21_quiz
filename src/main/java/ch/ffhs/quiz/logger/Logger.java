c1fff64728da2c0f642f9a03f2e741739992805c
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-06-01 19:44:54 +0200

    chore(Logger): Deprecated custom Logger

--- src/main/java/ch/ffhs/quiz/logger/Logger.java
+++ src/main/java/ch/ffhs/quiz/logger/Logger.java
@@ -3,6 +3,7 @@ package ch.ffhs.quiz.logger;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Deprecated(forRemoval = true)
 public class Logger {
     private static Date date = new Date();
     private static SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
