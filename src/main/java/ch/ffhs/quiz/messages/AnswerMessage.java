b6cdd5740fd2b9b30ef54b02889511312b606e17
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-06-01 22:15:23 +0200

    fix(AnswerMessage): Fixed null check

--- src/main/java/ch/ffhs/quiz/messages/AnswerMessage.java
+++ src/main/java/ch/ffhs/quiz/messages/AnswerMessage.java
@@ -1,14 +1,11 @@
 package ch.ffhs.quiz.messages;
 
-import java.util.Objects;
-
 public class AnswerMessage extends Message {
 
     private final int chosenAnswer;
 
     public AnswerMessage(int chosenAnswer){
         this.chosenAnswer = chosenAnswer;
-        Objects.requireNonNull(text);
         this.text = "";
     }
 
