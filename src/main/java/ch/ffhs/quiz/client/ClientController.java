39640e746f236fd53ee3e31d09537a292a4d92f5
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-01 17:59:11 +0200

    fix(ClientController.java): start refactoring, remove refactored methods

--- src/main/java/ch/ffhs/quiz/client/ClientController.java
+++ src/main/java/ch/ffhs/quiz/client/ClientController.java
@@ -1,18 +1,21 @@
 package ch.ffhs.quiz.client;
 
-import ch.ffhs.quiz.client.textinterface.*;
+import ch.ffhs.quiz.client.stages.InitializationStage;
+import ch.ffhs.quiz.messages.FeedbackMessage;
 import ch.ffhs.quiz.messages.Message;
-import ch.ffhs.quiz.messages.*;
+import ch.ffhs.quiz.messages.NameMessage;
+import ch.ffhs.quiz.messages.QuestionMessage;
 import ch.ffhs.quiz.server.MockServer;
 
 import java.io.IOException;
-import java.util.*;
 
-public class ClientController {
+public class ClientController{
     private static final InputHandler inputHandler = new InputHandler();
-    private static final Client client = new Client();
+    private static Client client;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        client = new Client("localhost", 3141);
+        new InitializationStage().process();
         new Thread(() -> {
             try {
                 MockServer.main(new String[0]);
@@ -20,27 +23,17 @@ public class ClientController {
                 ioException.printStackTrace();
             }
         });
-        handleConversation();
+        //handleConversation();
     }
 
     private static void handleConversation() {
         try {
 
-            String welcome = Colors.BLUE_BOLD.colorText(TextInterface.WELCOME.getComponent());
-            System.out.println(welcome);
-
-            System.out.println(TextInterface.EXPLANATION.getComponent());
 
             String userName = inputHandler.getUserName();
 
             client.connectToGameServer("localhost", 3141, new NameMessage(userName));
 
-            System.out.println("Sent name");
-
-            String answer = inputHandler.getUserAnswer();
-            List<Message> response = client.sendMessage(new AnswerMessage(answer));
-
-            response.forEach(ClientController::handleResponse);
 
         } catch(IOException | ClassNotFoundException ioEx){
             ioEx.printStackTrace();
