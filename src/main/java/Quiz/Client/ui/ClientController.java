938f5d69f4e9d42260824c5e8353f4e7994e585d
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-01 14:00:24 +0200

    feature(ui): merge with branch ui

--- src/main/java/Quiz/Client/ui/ClientController.java
+++
@@ -1,9 +0,0 @@
-package Quiz.Client.ui;
-
-
-public class ClientController {
-    public static void main(String[] args) {
-        String welcome = new AnsiBuilder(TextInterface.WELCOME.getComponent()).font(AnsiBuilder.Color.BLUE, true).create();
-        System.out.println(welcome);
-    }
-}
