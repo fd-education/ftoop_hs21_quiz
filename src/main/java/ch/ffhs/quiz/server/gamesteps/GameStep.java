d33eb4fbb1477b056db564623d550d3a2b6505b2
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-06-02 00:54:30 +0200

    feature(GameStep): Added logging

--- src/main/java/ch/ffhs/quiz/server/gamesteps/GameStep.java
+++ src/main/java/ch/ffhs/quiz/server/gamesteps/GameStep.java
@@ -1,8 +1,11 @@
 package ch.ffhs.quiz.server.gamesteps;
 
+import ch.ffhs.quiz.logger.LoggerUtils;
 import ch.ffhs.quiz.server.GameContext;
 import ch.ffhs.quiz.server.RoundContext;
 import ch.ffhs.quiz.server.player.Player;
+
+import java.util.logging.Logger;
 
 public abstract class GameStep {
     protected final GameContext gameContext;
@@ -16,9 +19,17 @@ public abstract class GameStep {
     protected abstract void handlePlayer(Player player);
     protected void completeStep() {}
     public final void process() {
+        Logger logger = LoggerUtils.getLogger();
+        logger.info("Starting game step: "+this.getClass().getSimpleName());
+        logger.info("Preparing game step");
         prepareStep();
-        for (Player player: gameContext.getPlayers())
+        logger.info("Handling players");
+        for (Player player: gameContext.getPlayers()) {
             handlePlayer(player);
+            logger.info("Handling player " + player.getId());
+        }
+        logger.info("Completing game step");
         completeStep();
+        logger.info("Game step completed!");
     }
 }
