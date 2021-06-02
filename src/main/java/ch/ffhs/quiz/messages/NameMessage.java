fd7d0eee4f34ca6d60c702e04bf9f4dc901468ed
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-03 00:22:23 +0200

    feature(Message): change validated to confirmed for more clarity

--- src/main/java/ch/ffhs/quiz/messages/NameMessage.java
+++ src/main/java/ch/ffhs/quiz/messages/NameMessage.java
@@ -3,18 +3,18 @@ package ch.ffhs.quiz.messages;
 import java.util.Objects;
 
 public class NameMessage extends Message {
-    private boolean validated;
+    private boolean confirmed;
 
     public NameMessage(String text) {
         Objects.requireNonNull(text);
         this.text = text;
     }
 
-    public void setValidated(boolean validated) {
-        this.validated = validated;
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
     }
 
-    public boolean isValidated(){
-        return validated;
+    public boolean isConfirmed(){
+        return confirmed;
     }
 }
