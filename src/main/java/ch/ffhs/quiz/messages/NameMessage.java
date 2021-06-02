edfcd90d396c99f9771db48339db0c02cc80eb49
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-03 00:20:13 +0200

    feature(Messages): add validation boolean with setter and getter to NameMessage

--- src/main/java/ch/ffhs/quiz/messages/NameMessage.java
+++ src/main/java/ch/ffhs/quiz/messages/NameMessage.java
@@ -3,9 +3,18 @@ package ch.ffhs.quiz.messages;
 import java.util.Objects;
 
 public class NameMessage extends Message {
+    private boolean validated;
 
     public NameMessage(String text) {
         Objects.requireNonNull(text);
         this.text = text;
+    }
+
+    public void setValidated(boolean validated) {
+        this.validated = validated;
+    }
+
+    public boolean isValidated(){
+        return validated;
     }
 }
