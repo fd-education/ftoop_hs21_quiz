9049da906c97dff22474cc3a354cb369dd9eca35
Sebastian Schneuwly <sebastian.schneuwly@students.ffhs.ch>
2021-06-04 17:19:13 +0200

    feature(AnswerImp):created a function, which returns a map, with the question as a key and a list of AnswerImp objects as a value

--- src/main/java/questions/AnswerImp.java
+++ src/main/java/questions/AnswerImp.java
@@ -1,64 +1,40 @@
 package questions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AnswerImp {
     String answer;
-    Boolean correctAnswer;
-//    public AnswerImp(String answer, Boolean correctAnswer) {
-//        this.answer = answer;
-//        this.correctAnswer = correctAnswer;
-//    }
-    public Map<Integer, List<String>> getAnswers(Map<String, List<String>> AnswerCatalog) {
-        Map<Integer, List<String>> mappedAnswers = new HashMap<>();
-        ArrayList<List<String>> answers = new ArrayList<>();
+    Boolean isCorrectAnswer;
 
-//        for(int i = 0; i < AnswerCatalog.size(); i++) {
-//            List<String> answersList = new ArrayList<>();
-//            for (int j = 0; j < AnswerCatalog.get(i).size(); j++) {
-//                String valueSecond = String.valueOf(AnswerCatalog.get(i).get(j).charAt(1));
-//                if (valueSecond.equals("*")) {
-//                    String correctAnswer = AnswerCatalog.get(i).get(j);
-//                    correctAnswer = correctAnswer.replace("*", "");
-//                    answersList.add(correctAnswer);
-//                } else {
-//                    answersList.add(AnswerCatalog.get(i).get(j));
-//                }
-//            }
-//            mappedAnswers.put(i, answersList);
-//        }
+    public Map<String, List<AnswerImp>> getAnswers(Map<String, List<String>> AnswerCatalog) {
+        Map<String , List<AnswerImp>> mappedAnswers = new HashMap<>();
 
-//        for(Map.Entry<String, List<String>> entry : AnswerCatalog.entrySet()) {
-//            List<String> list = entry.getValue();
-//            List<String> answersList = new ArrayList<>();
-//            int index = 0;
-//            for (int i = 0; i < list.size(); i++) {
-//
-//                String valueSecond = String.valueOf(list.get(i).charAt(1));
-//                if (valueSecond.equals("*")) {
-//                    String correctAnswer = list.get(i);
-//                    correctAnswer = correctAnswer.replace("*", "");
-//                    answersList.add(correctAnswer);
-//                } else {
-//                    answersList.add(list.get(i));
-//
-//                }
-//            }
-//
-//            mappedAnswers.put(index, answersList);
-//            index++;
-//        }
-        int index = 0;
-        for (List<String> values : AnswerCatalog.values()) {
-            mappedAnswers.put(index, values);
-            answers.add(values);
-            index++;
+        for (Map.Entry<String, List<String>> entry : AnswerCatalog.entrySet()) {
+            List<String> answers = new ArrayList<>();
+            List<AnswerImp> answerList = new ArrayList<>();
+            for (int i = 0; i < AnswerCatalog.get(entry.getKey()).size(); i++) {
+                String valueSecond = String.valueOf(AnswerCatalog.get(entry.getKey()).get(i).charAt(1));
+                if (valueSecond.equals("*")) {
+                    String correctAnswer = AnswerCatalog.get(entry.getKey()).get(i);
+                    correctAnswer = correctAnswer.replace("*", "");
+                    answers.add(correctAnswer);
+                    //AnswerImp answerImp = new AnswerImp(correctAnswer, true);
+                    AnswerImp answerImp = new AnswerImp();
+                    answerImp.answer = correctAnswer;
+                    answerImp.isCorrectAnswer = true;
+                    answerList.add(answerImp);
+                } else {
+                    String correctAnswer = AnswerCatalog.get(entry.getKey()).get(i);
+                    answers.add(correctAnswer);
+                    //AnswerImp answerImp = new AnswerImp(correctAnswer, false);
+                    AnswerImp answerImp = new AnswerImp();
+                    answerImp.answer = correctAnswer;
+                    answerImp.isCorrectAnswer = false;
+                    answerList.add(answerImp);
+                }
+            }
+            mappedAnswers.put(entry.getKey(), answerList);
         }
-        mapCorrectAnswers(answers);
-        System.out.println(mappedAnswers);
         return mappedAnswers;
     }
 
@@ -67,22 +43,6 @@ public class AnswerImp {
     }
 
     public boolean checkAnswer(){
-        return this.correctAnswer;
-    }
-
-    public Map<Integer, String> mapCorrectAnswers(ArrayList<List<String>> answers) {
-        Map<Integer, String> mappedCorrectAnswers = new HashMap<>();
-        for (int i = 0; i < answers.size(); i++) {
-            for (int j = 0; j < answers.get(i).size(); j++) {
-                String valueSecond = String.valueOf(answers.get(i).get(j).charAt(1));
-                if (valueSecond.equals("*")) {
-                    String correctAnswer = answers.get(i).get(j);
-                    correctAnswer = correctAnswer.replace("*", "");
-                    mappedCorrectAnswers.put(i, correctAnswer);
-                }
-            }
-        }
-        //System.out.println(mappedCorrectAnswers);
-        return mappedCorrectAnswers;
+        return this.isCorrectAnswer;
     }
 }
