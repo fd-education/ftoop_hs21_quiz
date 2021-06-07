e03e94d103b3a6212d88c97e7ee3e20334622df0
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-06-07 17:26:48 +0200

    feature(PlayerFactory): Now using logger

--- src/main/java/ch/ffhs/quiz/game/player/PlayerFactory.java
+++ src/main/java/ch/ffhs/quiz/game/player/PlayerFactory.java
@@ -2,6 +2,7 @@ package ch.ffhs.quiz.game.player;
 
 import ch.ffhs.quiz.connectivity.Connection;
 import ch.ffhs.quiz.connectivity.impl.ConnectionImpl;
+import ch.ffhs.quiz.logger.LoggerUtils;
 import ch.ffhs.quiz.server.Server;
 import ch.ffhs.quiz.game.player.impl.*;
 
@@ -10,21 +11,24 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public class PlayerFactory {
+    private static final Logger logger = LoggerUtils.getLogger();
+
     public static List<Player> connectPlayers(Server server, int playerCount) {
         Objects.requireNonNull(server);
 
         List<Player> players = new ArrayList<>();
-        for (int i = 0; i < playerCount; i++) {
+        for (int i = 0; i < playerCount;) {
             try {
                 Socket clientSocket = server.acceptConnection();
                 Connection connection = new ConnectionImpl(clientSocket.getOutputStream(), clientSocket.getInputStream());
                 PlayerData playerData = new PlayerDataImpl(i);
                 players.add(new PlayerImpl(playerData, connection));
+                i++;
             } catch (IOException e) {
-                System.err.printf("Player could not connect with error %s. Trying again...", e.getCause());
-                i--;
+                logger.warning("Player could not connect with error %s. Trying again...%n".formatted(e.getCause()));
             }
         }
         return players;
--- src/test/java/ch/ffhs/quiz/game/player/PlayerFactoryTest.java
+++ src/test/java/ch/ffhs/quiz/game/player/PlayerFactoryTest.java
@@ -1,30 +1,59 @@
 package ch.ffhs.quiz.game.player;
 
-import org.junit.jupiter.api.Test;
 import ch.ffhs.quiz.server.Server;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErr;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PlayerFactoryTest {
+    Server server;
+    Socket socket;
+    InputStream in;
+
+    @BeforeEach
+    void setup() throws IOException {
+        OutputStream out = mock(OutputStream.class);
+        in = mock(InputStream.class);
+        server = mock(Server.class);
+        socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(in);
+        when(socket.getOutputStream()).thenReturn(out);
+    }
 
     @Test
     void connectPlayers_positive_simple() throws IOException {
-        OutputStream out = mock(OutputStream.class);
-        InputStream in = mock(InputStream.class);
-        Server server = mock(Server.class);
-        Socket socket = mock(Socket.class);
-        when(socket.getInputStream()).thenReturn(in);
-        when(socket.getOutputStream()).thenReturn(out);
         when(server.acceptConnection()).thenReturn(socket);
 
         List<Player> players = PlayerFactory.connectPlayers(server, 2);
 
         assertEquals(0, players.get(0).getId());
         assertEquals(1, players.get(1).getId());
+    }
+
+    @Test
+    void connectPlayers_positive_throwingClientIsIgnored() throws Exception {
+        when(server.acceptConnection()).thenThrow(IOException.class).thenReturn(socket);
+        when(socket.getInputStream()).thenThrow(IOException.class).thenReturn(in);
+
+        String output = tapSystemErr(() -> {
+            List<Player> players = PlayerFactory.connectPlayers(server, 2);
+
+            assertEquals(0, players.get(0).getId());
+            assertEquals(1, players.get(1).getId());
+        });
+
+        assertNotEquals(output.indexOf("Player could not connect with error null. Trying again..."),
+                output.lastIndexOf("Player could not connect with error null. Trying again..."));
     }
 }
