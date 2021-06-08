abd6d5345170a50454225c355842e93297320fdc
Schneuwly Sebastian, GHR-OSA-NEX-18 <Sebastian.Schneuwly@swisscom.com>
2021-06-08 10:20:13 +0200

    fix(AnswerImp):cleanup

--- src/main/java/questions/AnswerImp.java
+++ src/main/java/questions/AnswerImp.java
@@ -11,33 +11,6 @@ public class AnswerImp implements Answer {
         this.isCorrectAnswer = isCorrectAnswer;
     }
 
- /*   public Map<String, List<AnswerImp>> getAnswers(Map<String, List<String>> AnswerCatalog) {
-        Map<String , List<AnswerImp>> mappedAnswers = new HashMap<>();
-
-        for (Map.Entry<String, List<String>> entry : AnswerCatalog.entrySet()) {
-            List<AnswerImp> answerList = new ArrayList<>();
-            for (int i = 0; i < AnswerCatalog.get(entry.getKey()).size(); i++) {
-                String valueSecond = String.valueOf(AnswerCatalog.get(entry.getKey()).get(i).charAt(1));
-                if (valueSecond.equals("*")) {
-                    String correctAnswer = AnswerCatalog.get(entry.getKey()).get(i);
-                    correctAnswer = correctAnswer.replace("*", "");
-                    AnswerImp answerImp = new AnswerImp();
-                    answerImp.answer = correctAnswer;
-                    answerImp.isCorrectAnswer = true;
-                    answerList.add(answerImp);
-                } else {
-                    String incorrectAnswer = AnswerCatalog.get(entry.getKey()).get(i);
-                    AnswerImp answerImp = new AnswerImp();
-                    answerImp.answer = incorrectAnswer;
-                    answerImp.isCorrectAnswer = false;
-                    answerList.add(answerImp);
-                }
-            }
-            mappedAnswers.put(entry.getKey(), answerList);
-        }
-        return mappedAnswers;
-    }*/
-
     @Override
     public boolean isCorrect() {
         return this.isCorrectAnswer;
