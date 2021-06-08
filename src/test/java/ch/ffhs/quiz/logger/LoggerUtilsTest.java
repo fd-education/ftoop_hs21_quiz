1a0e8adf48c98fc27abd9133e8e10e801d65fc9b
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2021-06-08 09:56:30 +0200

    feature(Logger): Added some tests

--- src/test/java/ch/ffhs/quiz/logger/LoggerUtilsTest.java
+++ src/test/java/ch/ffhs/quiz/logger/LoggerUtilsTest.java
@@ -3,8 +3,12 @@ package ch.ffhs.quiz.logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErr;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoggerUtilsTest {
@@ -21,8 +25,17 @@ class LoggerUtilsTest {
     }
 
     @Test
-    void logger_isCreated() {
+    void logger_positive_isCreated() {
         assertNotNull(logger);
         assertInstanceOf(Logger.class, logger);
+    }
+
+    @Test
+    void logger_positive_logsCorrectly() throws Exception {
+        String err = tapSystemErr((() -> logger.warning("errTest")));
+        String out = tapSystemOut((() -> logger.info("outTest")));
+
+        assertTrue(err.contains("errTest"));
+        assertTrue(out.contains("outTest"));
     }
 }
