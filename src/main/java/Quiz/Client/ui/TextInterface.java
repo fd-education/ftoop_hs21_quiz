53cac41f591f63a07a28804aa115e44bf3cee44f
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-09 22:23:02 +0200

    feature(TextInterface): add new text blocks

--- src/main/java/Quiz/Client/ui/TextInterface.java
+++ src/main/java/Quiz/Client/ui/TextInterface.java
@@ -2,12 +2,39 @@ package Quiz.Client.ui;
 
 public enum TextInterface {
     WELCOME("""
+            
             ███████╗ █████╗  ██████╗ █████╗ ██████╗ ███████╗     ██████╗ ██╗   ██╗██╗███████╗
             ██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝    ██╔═══██╗██║   ██║██║╚══███╔╝
             █████╗  ███████║██║     ███████║██║  ██║█████╗      ██║   ██║██║   ██║██║  ███╔╝
             ██╔══╝  ██╔══██║██║     ██╔══██║██║  ██║██╔══╝      ██║▄▄ ██║██║   ██║██║ ███╔╝
             ██║     ██║  ██║╚██████╗██║  ██║██████╔╝███████╗    ╚██████╔╝╚██████╔╝██║███████╗
             ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚══════╝     ╚══▀▀═╝  ╚═════╝ ╚═╝╚══════╝
+            """),
+    QUESTION("""
+             ██████╗ ██╗   ██╗███████╗███████╗████████╗██╗ ██████╗ ███╗   ██╗
+            ██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║
+            ██║   ██║██║   ██║█████╗  ███████╗   ██║   ██║██║   ██║██╔██╗ ██║
+            ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║   ██║   ██║██║   ██║██║╚██╗██║
+            ╚██████╔╝╚██████╔╝███████╗███████║   ██║   ██║╚██████╔╝██║ ╚████║
+             ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+            """),
+    END("""
+            ███████╗███╗   ██╗██████╗
+            ██╔════╝████╗  ██║██╔══██╗
+            █████╗  ██╔██╗ ██║██║  ██║
+            ██╔══╝  ██║╚██╗██║██║  ██║
+            ███████╗██║ ╚████║██████╔╝
+            ╚══════╝╚═╝  ╚═══╝╚═════╝
+            """),
+    EXPLANATION("""
+            Ein ganz normales Quiz... wie schwer kann das schon sein.
+            Ziel ist es, die richtige Antwort so schnell wie möglich zu finden 
+            und den entsprechenden Buchstaben einzutippen.
+            Vorsicht: Die erste Antwort zählt. Überleg dir also gut, ob du dir dabei sicher bist !
+    
+            Sollte niemand die Antwort wissen, geht die Runde nach 60 Sekunden vorbei. \u23F1
+    
+            Viel Spass (und Glück, falls du's nötig hast \uD83D\uDE09)!
             """);
 
     private final String component;
@@ -20,3 +47,4 @@ public enum TextInterface {
         return component;
     }
 }
+
