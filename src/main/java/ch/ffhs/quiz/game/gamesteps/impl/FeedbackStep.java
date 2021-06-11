9c6cb1ea0979e49aebb172041ea4b52dbbf8a10b
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-11 18:47:02 +0200

    refactor(FeedbackStep): move interface parts to client side

--- src/main/java/ch/ffhs/quiz/game/gamesteps/impl/FeedbackStep.java
+++ src/main/java/ch/ffhs/quiz/game/gamesteps/impl/FeedbackStep.java
@@ -21,21 +21,6 @@ public class FeedbackStep extends GameStep {
         final Player winningPlayer = roundContext.getWinningPlayer();
         final boolean wasCorrect = roundContext.wasPlayerCorrect(player);
 
-        // TODO: Transfer to client side
-//        if (wasCorrect) {
-//            if (winningPlayer.equals(player)) {
-//
-//                feedback = "You have won this round!";
-//            } else {
-//                feedback = "Your answer was correct, but Player %s was faster!".formatted(winningPlayer.getId());
-//            }
-//        } else {
-//            if (winningPlayer != null)
-//                feedback = "Your answer was not correct. Player %s won the round!".formatted(winningPlayer.getId());
-//            else
-//                feedback = "Nobody's answer was correct.";
-//        }
-
         boolean isFastestPlayer = false;
         String winningPlayerName = "";
         if (winningPlayer != null) {
