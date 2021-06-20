5b5d8c482ede61b7007858af76ee4fb1b363593e
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-20 02:45:00 +0200

    testing(TerminationStage): add tests for termination stage

--- client/src/test/java/ch/ffhs/quiz/client/stages/TerminationStageTest.java
+++ client/src/test/java/ch/ffhs/quiz/client/stages/TerminationStageTest.java
@@ -5,25 +5,29 @@ import ch.ffhs.quiz.client.InputHandler;
 import ch.ffhs.quiz.client.ui.UserInterface;
 import ch.ffhs.quiz.connectivity.Connection;
 import ch.ffhs.quiz.connectivity.impl.ConnectionImpl;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import ch.ffhs.quiz.logger.LoggerUtils;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
+import static org.mockito.Mockito.*;
+import org.mockito.MockedStatic;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-
-import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.logging.Logger;
 
 class TerminationStageTest {
     static ServerSocket server;
 
-    static Client client;
-    static Connection serverConnection;
-    static InputHandler inputHandler;
-    static UserInterface ui;
+    static Client client, mockClient;
+    static Connection serverConnection, mockConnection;
+    static InputHandler inputHandler, mockInputHandler;
+    static UserInterface ui, mockUi;
+    static Logger mockLogger;
 
-    static TerminationStage terminationStage;
+    static TerminationStage terminationStage, terminateStageMockedArguments, mockTermStage;
 
     @BeforeAll
     static void setup() throws Exception{
@@ -33,8 +37,16 @@ class TerminationStageTest {
         serverConnection = new ConnectionImpl(client.getOutputStream(), client.getInputStream());
         inputHandler = new InputHandler();
         ui = new UserInterface();
-
         terminationStage = new TerminationStage(client, serverConnection, inputHandler, ui);
+
+        mockLogger = mock(Logger.class);
+        mockClient = mock(Client.class);
+        mockConnection = mock(ConnectionImpl.class);
+        mockInputHandler = mock(InputHandler.class);
+        mockUi = mock(UserInterface.class);
+
+        terminateStageMockedArguments = new TerminationStage(mockClient, mockConnection, mockInputHandler, mockUi);
+        mockTermStage = mock(TerminationStage.class);
     }
 
 
@@ -88,7 +100,28 @@ class TerminationStageTest {
     }
 
     @Test
-    void createInitialUserInterface() throws Exception{
+    void setupStageTest_positive(){
+        terminationStage.setupStage();
+
+        assertInstanceOf(Logger.class, terminationStage.logger);
+    }
+
+    @Test
+    void setupStageTest_negative(){
+        try(MockedStatic<LoggerUtils> mockLoggerUtils = mockStatic(LoggerUtils.class)){
+            mockLoggerUtils.when(LoggerUtils::getUnnamedFileLogger).thenThrow(IOException.class);
+            assertThrows(RuntimeException.class, () -> terminateStageMockedArguments.setupStage());
+
+            try{
+                terminateStageMockedArguments.setupStage();
+            }catch(RuntimeException rtEx){
+                assertEquals("Could not instantiate the file logger. null", rtEx.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void createInitialUserInterfaceTest_positive() throws Exception{
         String EXPECTED = """
                 [H[2J[1;34m╔═══════════════════════════════════════════════════════════════════════════════════════════════════════╗
                 ╚███████████████████████████████████████████████████████████████████████████████████████████████████████╝
@@ -102,7 +135,34 @@ class TerminationStageTest {
                                                    ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝
                 [0m[2E[1;34m                           Danke für die Teilnahme an diesem Spiel ! ~N.S.F.[0m[H[2J""";
 
+
+        terminationStage.setupStage();
+        doNothing().when(mockLogger).info(anyString());
         String output = tapSystemOut(() -> terminationStage.createInitialUserInterface());
         assertEquals(EXPECTED, output);
+    }
+
+    @Test
+    void terminateStageTest_positive() throws IOException{
+        terminateStageMockedArguments.setupStage();
+
+        terminateStageMockedArguments.terminateStage();
+        verify(mockClient, times(1)).closeConnection();
+        verify(mockConnection, times(1)).close();
+    }
+
+    @Test
+    void terminateStageTest_negative() throws Exception{
+        int status = catchSystemExit(() -> {
+            terminateStageMockedArguments.setupStage();
+
+            doThrow(IOException.class).when(mockClient).closeConnection();
+            terminateStageMockedArguments.terminateStage();
+
+            verify(mockUi, times(1)).printErrorScreen();
+            verify(mockLogger, times(1)).warning(anyString());
+        });
+
+        assertEquals(-1, status);
     }
 }
