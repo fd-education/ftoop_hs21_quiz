c49a58acc17dd1e24b2229878db1a9ee27c0341c
Fabian Diemand <fabian.diemand@swisscom.com>
2021-06-20 23:03:54 +0200

    cleanup(ReadyMessage): add constructor to set ready

--- core/src/main/java/ch/ffhs/quiz/messages/ReadyMessage.java
+++ core/src/main/java/ch/ffhs/quiz/messages/ReadyMessage.java
@@ -4,12 +4,20 @@ package ch.ffhs.quiz.messages;
  * Used to signal a state of readiness.
  */
 public class ReadyMessage extends Message{
+    private final boolean ready;
+
+    public ReadyMessage(boolean ready){
+        this.ready = ready;
+    }
+
     /**
      * Always returns true to signal readiness.
      *
      * @return true
      */
+
+    @SuppressWarnings("SameReturnValue")
     public boolean isReady(){
-        return true;
+        return ready;
     }
 }
